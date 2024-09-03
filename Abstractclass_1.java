7a53521c211f2d3c9710a171857467c7fadbfe3d
venugopal <venu.play@gmail.com>
2024-09-03 17:52:03 +0530

    my java codes

---
+++ Abstractclass_1.java
@@ -0,0 +1,29 @@
+package abstractc;
+abstract class Amazom
+{
+	abstract   void login();
+	abstract  void registration();
+	}//exposing to 3rd part intigration 
+public class Abstractclass_1 extends Amazom
+{
+   static void logout() 
+   {
+	   
+   }
+   static void resetpassword() 
+   {
+	   
+   }
+	public static void main(String[] args) {
+
+	}
+	@Override
+	void login() {
+		
+	}
+	@Override
+	void registration() {
+		
+	}
+
+}
